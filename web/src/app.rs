0317c05550767cae3a86f146db5e67219ac010c8
Baraa Al-Masri <pub@mbaraa.com>
2024-01-21 02:22:26 +0300

    Add boilerpaltes

---
+++ web/src/app.rs
@@ -0,0 +1,10 @@
+use yew::{classes, function_component, html, Html};
+
+#[function_component(App)]
+pub fn app() -> Html {
+    html! {
+        <main style="min-height: 100dvh" class={classes!("bg-gray")}>
+        {"AAAAAAAAAAAAAAAAAAAAAAAA"}
+        </main>
+    }
+}
---
+++ web/tailwindcss/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["../**/*.{html,rs}"],
+  theme: {
+    extend: {
+      fontFamily: {},
+      colors: {
+        grey: "#191B1C",
+        blue: "#519AE5",
+        "dark-blue": "#1B222A",
+        white: "#E8E5E1",
+        black: "#000000",
+      },
+    },
+  },
+  plugins: [],
+};
