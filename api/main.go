d83a5652e8c23cf19a34e06c9afb0fa980d86dfe
Baraa Al-Masri <pub@mbaraa.com>
2024-01-27 03:51:47 +0300

    Add placeholder api

--- api/main.go
+++ api/main.go
@@ -1,10 +1,31 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
+type ResponeBody struct {
+	HtmxPfp string `json:"htmx_pfp"`
+}
+
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("/opt/dist")))
+	http.HandleFunc("/api/generate-htmx-pfp", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
+		if r.Method == http.MethodOptions {
+			return
+		}
+		req := map[string]any{}
+		_ = json.NewDecoder(r.Body).Decode(&req)
+		fmt.Println(req)
+		_ = json.NewEncoder(w).Encode(ResponeBody{
+			HtmxPfp: "AAAAAAA",
+		})
+	})
+
 	http.ListenAndServe(":8080", nil)
 }
